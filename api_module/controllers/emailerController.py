eaa29809438b8a73dd3fb3bc203ecd561fdab49c
mayank0-2 <mayank02091999@gmail.com>
2024-01-30 00:27:51 +0530

    fix

--- api_module/controllers/emailerController.py
+++ api_module/controllers/emailerController.py
@@ -7,7 +7,7 @@ from django.core.mail import EmailMessage, get_connection
 @shared_task
 def send_email(message) :
     html_message = open('email.html', 'r').read()
-    html = replace_text(html_message)
+    html = replace_text(html_message, message)
     msg = EmailMessage(subject, html, email_from,recipient_list, connection=connection) 
     msg.content_subtype = 'text/html'
     msg.send()
