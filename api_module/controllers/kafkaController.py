4d20786cf77736c4204ab12f93d25dae9b77ef7d
mayank0-2 <mayank02091999@gmail.com>
2024-03-17 13:27:05 +0530

    kafka

--- api_module/controllers/kafkaController.py
+++ api_module/controllers/kafkaController.py
@@ -2,13 +2,15 @@ from Email_campaign_manager.models import PlusUserDetail, PlusContent
 import socket, json
 from confluent_kafka import Producer, Consumer
 from api_module.controllers.emailerController import send_email
-from rest_framework.views import APIView
+from rest_framework.views import APIView, Response, status
+from api_module.response import ResponseFormat
+import os
 from config import ConfigVariables
 
 class KafkaProducer :
     def __init__(self):
-        conf = {'bootstrap.servers': "localhost:9092", 'client.id': socket.gethostname() }
-        self.producer = Producer(conf)
+        self.conf = {'bootstrap.servers': os.getenv('KAFKA_BROKER')}
+        self.producer = Producer(self.conf)
     
     def producer_message(self, topic, message) :
         user_data = PlusUserDetail.objects.all()
@@ -26,28 +28,32 @@ class KafkaProducer :
             }
             print(message)
             self.producer.produce(topic, key = None, value = json.dumps(message))
-            # self.producer.flush()
-        self.producer.close()
+            self.producer.flush()
+        # self.producer.close()
 
 
 
 class KafkaConsumer :
     def __init__(self) :
-        self.conf = {'bootstrap.servers': 'localhost:9092', 'group.id': 'foo','auto.offset.reset': 'smallest'}
-        self.consumer = Consumer(Conf)
+        self.conf = {'bootstrap.servers': os.getenv('KAFKA_BROKER'), 'group.id': 'foo','auto.offset.reset': 'smallest'}
+        self.consumer = Consumer(self.conf)
     
     def consumer_message(self, topic) :
         self.consumer.subscribe([topic])
         while True :
-            msg = self.consumer.poll(ConfigVariables().poll_time) #derive from config
+            print('inside the while ')
+            msg = self.consumer.poll(timeout = ConfigVariables().poll_time) #derive from config
+            print(msg)
             if msg is None :
+                print("Consumer timeout. Continuing.")
                 continue
             if msg.error() :
                 print("Consumer error: {}".format(msg.error()))
                 continue
             message = msg.value().decode('utf-8')
-            send_email.delay(message)
-            self.consumer.close()
+            # send_email.delay(message)
+            print(message)
+            # self.consumer.close()
 
 
 class exposeEndpoint(APIView):
